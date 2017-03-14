5ce78ba09c3302639f058e28649842d89a20b91d
David Stancu <dstancu@nyu.edu>
2017-03-14 15:42:32 -0400

    rewrite another awful-ish test, tfw looking at old code

--- spec/config/wrapper_spec.rb
+++ spec/config/wrapper_spec.rb
@@ -13,22 +13,20 @@ describe Lazywake::Config::Wrapper do
 
   let(:path) { File.join(Dir.home, '.lazywake') }
 
-  before do
-    FileUtils.rm_rf(path)
-    described_class.reset
-  end
+  before { described_class.reset }
 
   context '.load' do
     let(:invalid_config_file) { 'alphabet soup' }
 
     it 'parses a valid config file' do
-      File.open(path, 'w+') { |f| f.write(valid_config_file) }
+      expect(File).to receive(:read).with(path).and_return(valid_config_file)
       described_class.load(path)
       expect(described_class.data).to be_a(Hash)
     end
 
     it 'fails with an invalid config' do
-      File.open(path, 'w+') { |f| f.write(invalid_config_file) }
+      expect(File).to receive(:read).with(path).and_return(invalid_config_file)
+
       expect { described_class.load(path) }
         .to raise_error(Lazywake::Config::Schema::ConfigValidationError)
       expect(described_class.data).to be(nil)
@@ -37,17 +35,17 @@ describe Lazywake::Config::Wrapper do
 
   context '.save' do
     before do
-      File.open(path, 'w+') { |f| f.write(valid_config_file) }
+      expect(File).to receive(:read).with(path).and_return(valid_config_file)
       described_class.load(path)
     end
 
     it 'will save valid changes to the config' do
       Lazywake::Config.generated_mappings[:new_entry] = '12:34:56:78:90:11'
-      described_class.save(path)
 
-      expect(
-        JSON.parse(File.read(path)).fetch('generated_mappings')
-      ).to eql Lazywake::Config.generated_mappings
+      expect_any_instance_of(File).to receive(:write)
+        .with(Lazywake::Config.data.to_json)
+        
+      described_class.save(path)
     end
 
     it 'fails with an invalid field value' do
